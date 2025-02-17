04b732c0466d06195712e45c7e101be852a62e5f
Kevin Cazelles <kevin.cazelles@insileco.io>
2025-02-17 15:55:52 -0500

    CST-455 Update Shiny App with new results

--- ShinyApp/App.R
+++ ShinyApp/App.R
@@ -1,41 +1,50 @@
 library(shiny)
-library(shinythemes)
 library(shinybusy)
+library(shinyjs)
 library(leaflet)
+library(bslib)
 
+# Geometries
+## Canada (with results)
+map6 <- sf::read_sf("s_map6.gpkg")
+## Lake Erie (with results)
+mapl <- sf::read_sf("s_mapl.gpkg")
+## FEOW
+feow <- sf::read_sf("s_feow.gpkg")
 
 ############
 ui <- navbarPage(
-  theme = shinytheme("simplex"),
+  theme = bs_theme(version = 5),
+  useShinyjs(),
   title = "Watershed prioritization in Canada",
   #
-  # tags$style(type='text/css', ".selectize-input { font-size: 11px; line-height: 11px;} .selectize-dropdown { font-size: 11px; line-height: 11px; }"),
-
   tabPanel(
     "Map",
-
     # sidebar
     sidebarLayout(
       sidebarPanel(
         width = 4,
-        p(strong("Prioritizing watersheds for conservation")),
+        h5(strong("Prioritizing watersheds for conservation")),
         splitLayout(
           selectInput(
             inputId = "scale", label = "Watershed scale",
-            choices = c("Hydrobasin level 5", "Hydrobasin level 6", "Lake Erie")
+            choices = c("Hydrobasin level 6", "Lake Erie")
           ),
-          selectInput(inputId = "objective", label = "Weighting schema", choices = c(
-            "Custom", "Area-based protection", "Restoration", "SAR management", "AIS management"
-          )),
+          div(
+            id = "within_feow",
+            class = "my-3 pt-4 mx-3",
+            checkboxInput("regional", label = "Watersheds ranked within ecoregions?", value = TRUE)
+          ),
           tags$head(tags$style(HTML(".shiny-split-layout > div {overflow: visible;}"))),
-          cellWidths = c("45%", "55%")
+          cellWidths = c("40%", "60%")
         ),
-        splitLayout(
-          actionButton(inputId = "go", label = "Generate map"),
-          actionButton(inputId = "reset", label = "Reset sliders")
+        selectInput(inputId = "objective", label = "Weighting schema", choices = c(
+          "Custom", "Area-based protection", "Restoration", "SAR management", "AIS management"
+        )),
+        div(
+          class = "my-4",
+          h5(strong("Weight watersheds according to:"))
         ),
-        checkboxInput("regional", label = "Watersheds ranked within ecoregions?", value = TRUE),
-        p(strong("Weight watersheds according to:")),
         sliderInput(
           inputId = "WSI",
           label = "Watershed stress",
@@ -64,6 +73,11 @@ ui <- navbarPage(
           inputId = "CCI",
           label = "Climate change",
           min = -5, max = 5, value = 0, step = 0.1
+        ),
+        div(
+          class = "d-flex d-flex justify-content-between",
+          actionButton(inputId = "reset", icon = icon("refresh"), label = "Reset sliders"),
+          actionButton(inputId = "go", icon = icon("map"), label = "Generate map")
         )
       ),
       mainPanel( # main section of Interactive map tab (i.e. the map)
@@ -72,71 +86,64 @@ ui <- navbarPage(
       ) # end of main panel
     ) # end of sideBarLayout
   ), # end of Tab
-
   tabPanel(
     "Details",
-    p("This app ranks watersheds for conservation objectives based on the methods outlined in [PAPER REFERENCE]"),
-    hr(),
-    p(
-      strong("Watershed scale"),
-      br(),
-      "The unit of watersheds at which the prioritization is done. Options include Hydrobasin level 5 and 6 from SOURCE. Level 5 watersheds are larger than level 6. "
-    ),
-    br(),
-    p(
-      strong("Weighting schema"),
-      br(),
-      "Create a custom weighting scheme, or select one of the schemes meant to select watersheds for area-based protection, restoration, species at risk management or invasive species management from PAPER REFERENCE"
-    ),
-    hr(),
-    p(em("Select weights for the 6 variables below to help identify priority watersheds for conservation. Positive weight vales (i.e. > 0) will select watersheds with large values of the variable,
-      while negative weight values (i.e. < 0) will select watersheds with low values of the variable. Weight values of 0 indicate that the variable should be ignored when selecting watersheds.")),
-    p(
-      strong("Watershed stress"),
-      br(),
-      "Intensity of human impact in a watershed based on the Global Human Modification dataset (Theobald et al. 2017. Earth Sci Syst Data). Normalized range of 0-100"
-    ),
-    br(),
-    p(
-      strong("Fish species at risk (SAR) richness"),
-      br(),
-      "Number of COSEWIC listed fish species at risk with a watershed. Normalized range of 0-100"
-    ),
-    br(),
-    p(
-      strong("Fish species richness"),
-      br(),
-      "Number of fish species (including native, foreign and translocated species) within a watershed. Normalized range of 0-100"
-    ),
-    br(),
-    p(
-      strong("Fish biodiversity change"),
-      br(),
-      "Index of the relative rarity of fish species within in the amount, from Minns (1987) NZ J Zool. Normalized range of 0-100"
-    ),
-    br(),
-    p(
-      strong("Fish biodiversity change"),
-      br(),
-      "Index of the amount of change in a fish community between historical (ie 1960s) and current periods. Most of this change is driven by invasive species. Normalized range of 0-100"
-    ),
-    br(),
-    p(
-      strong("Climate change"),
-      br(),
-      "Index of the amount of climate change predicted in a watershed by 2055 under an RCP 4.5 scenario. Normalized range of 0-100"
-    ),
-    hr(),
-    p(
-      strong("Watersheds ranked within ecoregions?"),
-      br(),
-      "Select whether you want watersheds to be ranked within their freshwater ecoregion (i.e. high rankings = high for that ecoregion). Default (unchecked) value is that rankings are conducted at a national scale."
+    div(
+      class = "d-flex justify-content-center",
+      div(
+        class = "w-50 p-3",
+        includeHTML("details.html")
+      )
     )
   )
 )
 
 ############
 server <- function(input, output, session) {
+  # -------- Hide button if Lake Erie
+  observe({
+    if (input$scale == "Lake Erie") {
+      hide("within_feow")
+    } else {
+      show("within_feow")
+    }
+  })
+
+  # -------- Weighting schema
+  cursor_ref <- reactive({
+    switch(input$objective,
+      "Area-based protection" = c(-1, 1.1, 1, 1.7, -1.1, -0.5),
+      "Restoration" = c(2.5, 1.4, 1, 1.2, 0.1, -1),
+      "SAR management" = c(1.5, 2.1, 0, 1.3, 1.3, 1),
+      "AIS management" = c(1.4, 1, 1, 1, 1.8, 0.5),
+      "Custom" = rep(0, 6),
+      stop("Unknown schema")
+    )
+  })
+
+  observe({
+    curs <- cursor_ref()
+    updateSliderInput(inputId = "WSI", value = curs[1])
+    updateSliderInput(inputId = "SARI", value = curs[2])
+    updateSliderInput(inputId = "Richness", value = curs[3])
+    updateSliderInput(inputId = "Q", value = curs[4])
+    updateSliderInput(inputId = "FBCI", value = curs[5])
+    updateSliderInput(inputId = "CCI", value = curs[6])
+  })
+
+  # Reset sliders buttons
+  observeEvent(input$reset, {
+    curs <- cursor_ref()
+    updateSliderInput(inputId = "WSI", value = curs[1])
+    updateSliderInput(inputId = "SARI", value = curs[2])
+    updateSliderInput(inputId = "Richness", value = curs[3])
+    updateSliderInput(inputId = "Q", value = curs[4])
+    updateSliderInput(inputId = "FBCI", value = curs[5])
+    updateSliderInput(inputId = "CCI", value = curs[6])
+  })
+
+
+  # -------- Map
   # load initial map
   output$map <- renderLeaflet({
     leaflet(options = leafletOptions(preferCanvas = TRUE)) |>
@@ -149,142 +156,74 @@ server <- function(input, output, session) {
       leafem::addMouseCoordinates() |>
       fitBounds(lat1 = 43, lng1 = -143, lat2 = 75, lng2 = -50)
   })
-
-  # Reset sliders buttons
-  observeEvent(input$reset, {
-    if (input$objective == "Area-based protection") {
-      updateSliderInput(inputId = "WSI", value = -1)
-      updateSliderInput(inputId = "SARI", value = 1.1)
-      updateSliderInput(inputId = "Richness", value = 1)
-      updateSliderInput(inputId = "Q", value = 1.7)
-      updateSliderInput(inputId = "FBCI", value = -1.1)
-      updateSliderInput(inputId = "CCI", value = -0.5)
-    }
-    if (input$objective == "Restoration") {
-      updateSliderInput(inputId = "WSI", value = 2.5)
-      updateSliderInput(inputId = "SARI", value = 1.4)
-      updateSliderInput(inputId = "Richness", value = 1)
-      updateSliderInput(inputId = "Q", value = 1.2)
-      updateSliderInput(inputId = "FBCI", value = 0.1)
-      updateSliderInput(inputId = "CCI", value = -1)
-    }
-    if (input$objective == "SAR management") {
-      updateSliderInput(inputId = "WSI", value = 1.5)
-      updateSliderInput(inputId = "SARI", value = 2.1)
-      updateSliderInput(inputId = "Richness", value = 0)
-      updateSliderInput(inputId = "Q", value = 1.3)
-      updateSliderInput(inputId = "FBCI", value = 1.3)
-      updateSliderInput(inputId = "CCI", value = 1)
-    }
-    if (input$objective == "AIS management") {
-      updateSliderInput(inputId = "WSI", value = 1.4)
-      updateSliderInput(inputId = "SARI", value = 1)
-      updateSliderInput(inputId = "Richness", value = 1)
-      updateSliderInput(inputId = "Q", value = 1)
-      updateSliderInput(inputId = "FBCI", value = 1.8)
-      updateSliderInput(inputId = "CCI", value = 0.5)
-    }
-    if (input$objective == "Custom") {
-      updateSliderInput(inputId = "WSI", value = 0)
-      updateSliderInput(inputId = "SARI", value = 0)
-      updateSliderInput(inputId = "Richness", value = 0)
-      updateSliderInput(inputId = "Q", value = 0)
-      updateSliderInput(inputId = "FBCI", value = 0)
-      updateSliderInput(inputId = "CCI", value = 0)
-    }
-  })
-
-
-  # update sliders based on objective
-  observeEvent(input$objective, {
-    if (input$objective == "Area-based protection") {
-      updateSliderInput(inputId = "WSI", value = -1)
-      updateSliderInput(inputId = "SARI", value = 1.1)
-      updateSliderInput(inputId = "Richness", value = 1)
-      updateSliderInput(inputId = "Q", value = 1.7)
-      updateSliderInput(inputId = "FBCI", value = -1.1)
-      updateSliderInput(inputId = "CCI", value = -0.5)
-    }
-    if (input$objective == "Restoration") {
-      updateSliderInput(inputId = "WSI", value = 2.5)
-      updateSliderInput(inputId = "SARI", value = 1.4)
-      updateSliderInput(inputId = "Richness", value = 1)
-      updateSliderInput(inputId = "Q", value = 1.2)
-      updateSliderInput(inputId = "FBCI", value = 0.1)
-      updateSliderInput(inputId = "CCI", value = -1)
-    }
-    if (input$objective == "SAR management") {
-      updateSliderInput(inputId = "WSI", value = 1.5)
-      updateSliderInput(inputId = "SARI", value = 2.1)
-      updateSliderInput(inputId = "Richness", value = 0)
-      updateSliderInput(inputId = "Q", value = 1.3)
-      updateSliderInput(inputId = "FBCI", value = 1.3)
-      updateSliderInput(inputId = "CCI", value = 1)
-    }
-    if (input$objective == "AIS management") {
-      updateSliderInput(inputId = "WSI", value = 1.4)
-      updateSliderInput(inputId = "SARI", value = 1)
-      updateSliderInput(inputId = "Richness", value = 1)
-      updateSliderInput(inputId = "Q", value = 1)
-      updateSliderInput(inputId = "FBCI", value = 1.8)
-      updateSliderInput(inputId = "CCI", value = 0.5)
-    }
-    if (input$objective == "Custom") {
-      updateSliderInput(inputId = "WSI", value = 0)
-      updateSliderInput(inputId = "SARI", value = 0)
-      updateSliderInput(inputId = "Richness", value = 0)
-      updateSliderInput(inputId = "Q", value = 0)
-      updateSliderInput(inputId = "FBCI", value = 0)
-      updateSliderInput(inputId = "CCI", value = 0)
-    }
-  })
-
-  data5 <- readr::read_csv(
-    "watershed_prioritization_level5.csv",
-    show_col_types = FALSE
-  ) |>
-    dplyr::select(
-      HYBAS_ID, FEOW_ID, WSI_n, FBCI_n, CCI_n, SARI_n, Fish_richness_n, Priority_n
-    )
-  data6 <- readr::read_csv(
-    "watershed_prioritization_level6.csv",
-    show_col_types = FALSE
-  ) |>
-    dplyr::select(
-      HYBAS_ID, FEOW_ID, WSI_n, FBCI_n, CCI_n, SARI_n, Fish_richness_n, Priority_n
-    )
-  #
-  map5 <- sf::read_sf("s_map5.gpkg")
-  map6 <- sf::read_sf("s_map6.gpkg")
-  mapl <- sf::read_sf("s_lakeerie.gpkg")
-  feow <- sf::read_sf("s_feow.gpkg")
-
-
-  # Generate map button
+  # Generate map
   observeEvent(input$go, {
     show_modal_spinner() # show the modal window
 
-    if (input$scale == "Hydrobasin level 5") {
-      map <- map5
-      data <- data5
-    } else if (input$scale == "Hydrobasin level 6") {
+    if (input$scale == "Hydrobasin level 6") {
       map <- map6
-      data <- data6
     } else {
       map <- mapl
     }
     bbox <- sf::st_bbox(map)
-    print(bbox)
 
-    # calculate the ranks based on user input weightings
+    map <- map |>
+      dplyr::mutate(
+        score =
+          WSI_n * input$WSI +
+            FBCI_n * input$FBCI +
+            CCI_n * input$CCI +
+            SARI_n * input$SARI +
+            Fish_richness_n * input$Richness +
+            Priority_n * input$Q
+      )
+
+    # popup labels for watersheds
+    wsh_labels <- lapply(seq(nrow(map)), function(i) {
+      paste0(
+        "Watershed stress: ", round(map$WSI_n[i], 2), "<br>",
+        "SAR richness: ", round(map$SARI_n[i], 2), "<br>",
+        "Fish species richness: ", round(map$Fish_richness_n[i], 2), "<br>",
+        "Biodiversity change: ", round(map$FBCI_n[i], 2), "<br>",
+        "Fish rarity: ", round(map$Priority_n[i], 2), "<br>",
+        "Climate change: ", round(map$CCI_n[i], 2), "<br>"
+      )
+    })
+
     if (input$scale == "Lake Erie") {
+      map <- map |>
+        dplyr::mutate(watershed_rank = rank(-score))
+
+      pal <- viridis::viridis(max(map$watershed_rank),
+        option = "magma",
+        direction = -1,
+        end = 0.9
+      )
+
+      map$wsh_fill <- pal[map$watershed_rank]
+
       leafletProxy("map") |>
-        clearGroup(group = "watersheds") |>
+        removeLayersControl() |>
         clearShapes() |>
         clearControls() |>
         leafem::addMouseCoordinates() |>
         addPolygons(
-          data = map
+          data = map,
+          fillColor = map$wsh_fill,
+          color = map$wsh_fill,
+          opacity = 0.6,
+          weight = 2,
+          label = lapply(wsh_labels, htmltools::HTML),
+          fillOpacity = 0.8,
+          highlightOptions = highlightOptions(
+            color = "white", weight = 2,
+            bringToFront = TRUE
+          )
+        ) |>
+        addLegend(
+          colors = viridis::viridis(9, alpha = 1, end = 0.9, direction = -1, option = "magma"),
+          labels = c("high", rep("", 7), "low"),
+          title = "Priority", group = "watersheds"
         ) |>
         leaflet::setView(
           lng = mean(bbox[c(1, 3)]),
@@ -292,66 +231,35 @@ server <- function(input, output, session) {
           zoom = 8
         )
     } else {
-      data <- data |>
-        dplyr::mutate(
-          score =
-            WSI_n * input$WSI +
-              FBCI_n * input$FBCI +
-              CCI_n * input$CCI +
-              SARI_n * input$SARI +
-              Fish_richness_n * input$Richness +
-              Priority_n * input$Q
-        )
-
       if (!input$regional) {
-        data <- data |>
+        map <- map |>
           dplyr::mutate(watershed_rank = rank(-score))
       } else {
-        data <- data |>
+        map <- map |>
           dplyr::arrange(FEOW_ID, -score) |>
           dplyr::group_by(FEOW_ID) |>
           dplyr::mutate(watershed_rank = dplyr::row_number())
 
         # convert to relative ranks
-        maxrank <- max(data$watershed_rank - 1)
-        data <- data |>
+        maxrank <- max(map$watershed_rank - 1)
+        map <- map |>
           dplyr::group_by(FEOW_ID) |>
           dplyr::mutate(watershed_rank = ((watershed_rank - 1) * maxrank / max(watershed_rank - 1)) + 1)
 
-        data$watershed_rank[which(is.nan(data$watershed_rank))] <- (maxrank + 1) / 2 # if there is only 1 watershed in feow
+        map$watershed_rank[which(is.nan(map$watershed_rank))] <- (maxrank + 1) / 2
+        # if there is only 1 watershed in feow
       }
 
 
-      pal <- viridis::viridis(max(data$watershed_rank),
+      pal <- viridis::viridis(max(map$watershed_rank),
         option = "magma",
         direction = -1,
         end = 0.9
       )
-
-      data$wsh_fill <- pal[data$watershed_rank]
-
-      # join the data with the map
-      # map = map |> select(HYBAS_ID, geometry)
-      map <- map |>
-        dplyr::select(HYBAS_ID, geom) |>
-        dplyr::left_join(data, by = "HYBAS_ID")
-
-      # popup labels for watersheds
-      wsh_labels <- lapply(seq(nrow(map)), function(i) {
-        paste0(
-          "Watershed stress: ", round(map$WSI_n[i], 2), "<br>",
-          "SAR richness: ", round(map$SARI_n[i], 2), "<br>",
-          "Fish species richness: ", round(map$Fish_richness_n[i], 2), "<br>",
-          "Biodiversity change: ", round(map$FBCI_n[i], 2), "<br>",
-          "Fish rarity: ", round(map$Priority_n[i], 2), "<br>",
-          "Climate change: ", round(map$CCI_n[i], 2), "<br>"
-        )
-      })
-
+      map$wsh_fill <- pal[map$watershed_rank]
 
       # map it
       leafletProxy("map") |>
-        clearGroup(group = "watersheds") |>
         clearShapes() |>
         clearControls() |>
         addPolygons(
@@ -390,6 +298,7 @@ server <- function(input, output, session) {
         ) |>
         fitBounds(lat1 = 43, lng1 = -143, lat2 = 75, lng2 = -50)
     }
+
     remove_modal_spinner() # remove it when done
   }) # end of 'go' button
 } # end of server
