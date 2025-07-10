68677c96e2720fe003ed0e555fd3c1cc188edea1
Kevin Cazelles <kevin.cazelles@insileco.io>
2025-07-10 13:16:16 -0400

    CST-567 add new lake Erie data

--- R/spatialize_results.R
+++ R/spatialize_results.R
@@ -1,20 +1,22 @@
 #' Generates a sf object including results.
 #'
 #' @param x `[data.frame]`\cr A data frame including the results.
+#' @param scale [character string]`\cr Either "national" or "lake erie".#
 #'
 #' @details
 #' Use the number of rows to determine whether the geometry of Lake Erie or the
 #' entire Canada should be used.
 #'
 #' @export
-spatialize_results <- function(x) {
-    if (nrow(x) > 1e3) {
-        path_input_data("map6.gpkg") |>
+spatialize_results <- function(x, scale = c("national", "lake erie")) {
+    scale <- match.arg(scale)
+    switch(scale,
+        "national" = path_input_data("map6.gpkg") |>
             sf::read_sf() |>
-            dplyr::inner_join(x, by = dplyr::join_by(HYBAS_ID, FEOW_ID))
-    } else {
-        m_path <- path_input_data("lakeErieH12.gpkg", lvl = "H12_LakeErie") |>
+            dplyr::inner_join(x, by = dplyr::join_by(HYBAS_ID, FEOW_ID)),
+        "lake erie" = path_input_data("lakeErieH12.gpkg", lvl = "H12_LakeErie") |>
             sf::read_sf() |>
-            dplyr::inner_join(x, by = dplyr::join_by(HYBAS_ID))
-    }
+            dplyr::inner_join(x, by = dplyr::join_by(HYBAS_ID)),
+        stop("Unknown scale")
+    )
 }
